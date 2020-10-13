461e491be10dd288e265776d1624752f0bf69ae0
Amir Amram <Amir.Amram14@gmail.com>
2020-10-13 14:33:40 +0300

    input

--- question-1/question-1.py
+++ question-1/question-1.py
@@ -1,1 +1,20 @@
-print("kukuricoo")
+import random
+
+
+def input_random(n = 9, m = 9, r = 1):
+    print("\n", n, m, r, end = "\n\n")
+    for i in range(0,int(n)):
+        for j in range(0,int(m)):
+            print(random.randint(0,9), end = " ")
+        print("")
+         
+
+def main():
+    input_random()
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
