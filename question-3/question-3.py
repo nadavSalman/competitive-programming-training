c33ebfad594a9479280c5188b0262fbd30eaf7d7
Amir Amram <amir.amram14@gmail.com>
2020-11-29 09:17:29 +0200

    list zogi e-zogi divider func has created

--- question-3/question-3.py
+++ question-3/question-3.py
@@ -5,7 +5,7 @@ B_SIZE = 2*A_SIZE - 1
 
 def define_ab():
     
-    a = [0,4,6,7]
+    a = [0,0,1,3]
     b = [0] * (B_SIZE +1)
     
     for i in range(1,A_SIZE+1):
@@ -40,8 +40,31 @@ def create_sequnce(a, options):
             sequnce.append(a[i+1] + j)
         sequnces.append(sequnce)
     print(sequnces) 
+    return sequnces
     
+def sort_zogi_e_zogi(sequnces):
+
+    l_zogi = []
+    l_e_zogi = []
+   
+
+    for item in sequnces:
     
+        l_s_zogi = []
+        l_s_e_zogi = []
+   
+        for i in range(0,len(item)):
+            if item[i]%2 == 0:
+                l_s_zogi.append(item[i])
+            else:
+                l_s_e_zogi.append(item[i])
+                
+        l_e_zogi.append(l_s_e_zogi)
+        l_zogi.append(l_s_zogi)
+        
+    print(l_e_zogi,'\n',l_zogi)
+    return l_zogi, l_e_zogi
+
 
  
 def condition():
@@ -54,7 +77,10 @@ def condition():
 def main():
     a,b = define_ab()
     xor_a(a)
-    create_sequnce(a, options(a))
+    sequnces = create_sequnce(a, options(a))
+    
+    zogi, e_zogi = sort_zogi_e_zogi(sequnces)
+   
 
 if __name__ == "__main__":
     main()
