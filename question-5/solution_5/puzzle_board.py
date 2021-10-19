fa1c7ee7c4e45edab86ae909373c57cbb54f2ab5
nadavSalman <nadavops@gmail.com>
2021-10-19 18:23:40 +0300

    update Class PuzzleBoard functinalty to generate boad with numpy

--- question-5/solution_5/puzzle_board.py
+++ question-5/solution_5/puzzle_board.py
@@ -1,5 +1,4 @@
-
-
+import numpy as np
 
 
 class PuzzleBoard(object):
@@ -7,10 +6,27 @@ class PuzzleBoard(object):
 
 
     def __init__(self, board_cells_types,n_rows,m_columns):
-        self.board_cells_types = board_cells_types
+        self.board_cells_types = board_cells_types #feature stull deeactivate 
         self.n_rows = n_rows
         self.m_columns = m_columns
+        self.board = np.zeros((self.n_rows,self.m_columns)).astype(int)
 
 
+    def __str__(self) -> str:
+        #return super().__str__()
+        return "PuzzleBoard object data : {} {} {}".format(self.board_cells_types,self.n_rows,self.m_columns)
+
+
+    '''
+    A get by reff funciton
+    '''
+    def get_boad(self):
+        return self.board
+
+    '''
+    A get by value 
+    '''
+    def get_board_copy(self):
+        pass
 
 
--- question-5/solution_5/solution.py
+++ question-5/solution_5/solution.py
@@ -1,14 +1,30 @@
 import numpy
-
+from puzzle_board import PuzzleBoard
 
 
 
 
 def main():
-    # Array of zeros and ones:
-    # zeors_array = numpy.zeros((2, 3))
-    # print(zeors_array)
     print('solution 5')
+    
+    '''
+    Generate matrix of type int :
+        [[0 0 0 0 0 0 0]
+        [0 0 0 0 0 0 0]
+        [0 0 0 0 0 0 0]
+        [0 0 0 0 0 0 0]
+        [0 0 0 0 0 0 0]]
+    '''
+    zeors_array = numpy.zeros((5,7)).astype(int)
+    #print(zeors_array)
+    nadav_board = PuzzleBoard(int,6,9)
+    print(nadav_board)
+    nadav_board.get_boad()[5][6] = 1
+    print(nadav_board.get_boad())
+
+
+
+    
 
 
 
