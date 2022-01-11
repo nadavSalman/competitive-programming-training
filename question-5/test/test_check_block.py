1502a16bce2db9f98df528fe2c39ebffa45a056c
nadavSalman <nadavops@gmail.com>
2022-01-11 08:21:09 +0200

    update ubuti test in question-5

--- question-5/test/test_check_block.py
+++ question-5/test/test_check_block.py
@@ -4,7 +4,7 @@ import unittest
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))#Add to path parent dir.
 
-from solution_5.solution import PuzzleSolution
+from solution import PuzzleSolution
 
 class TestCheckBlock(unittest.TestCase):
 
@@ -22,11 +22,10 @@ class TestCheckBlock(unittest.TestCase):
 
     
     def test_check_block_contain_one_sand_block(self):
-        
         row = 5
         column = 7
-        game_board.get_board()[row][column]  = 1
-        self.assertEquals(game_board.check_block(row,column),True)
+        self.game_board.get_board()[row][column]  = 1
+        self.assertEquals(self.game_board.check_block(row,column),True)
     
 
 
