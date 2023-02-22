776a1785e5d6133973b421455261a14beb6f2980
nsalman <nsalman@attentigroup.com>
2023-02-22 10:14:28 +0200

    Create Solution

--- question-7/solution_7/solution.py
+++ question-7/solution_7/solution.py
@@ -9,23 +9,52 @@
 # '1101110'
 # >>> 
 
-def equal_sub_binary(dec_value,power,binary_input):
-    groups_count = 0
-    sub_binary = ''
-    for index in range(0,binary_input):
-        if binary_input[index] == '1':
-            sub_binary += '1'
-            if int(sub_binary) ==  dec_value:
-                groups_count += 1
-                 
 
+
+
+
+
+
+
+def equal_binary_parts(Z):
+    # Compute the total number of ones in the input list
+    total_ones = sum(Z)
+
+    # If the total number of ones is not divisible by 3, then no solution exists
+    if total_ones % 3 != 0:
+        return None
+
+    # Compute the number of ones that should be present in each part
+    ones_per_part = total_ones // 3
+
+    # Initialize counters for the number of ones seen so far and the current part number
+    ones_seen = 0
+    part_num = 0
+
+    # Iterate over the input list to find the indices that separate the three parts
+    i, j = None, None
+    for idx, val in enumerate(Z):
+        ones_seen += val
+        if ones_seen == ones_per_part * (part_num + 1):
+            if part_num == 0:
+                i = idx
+            elif part_num == 1:
+                j = idx
+            part_num += 1
+
+    # If we found all three parts, return their indices
+    if i is not None and j is not None:
+        return [i, j]
+
+    # Otherwise, no solution exists
+    return None
 
 
         
+def main(): 
+    z = [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
+    print(equal_binary_parts(z))
 
-
-def main():
-    print('sdfsd')
 
 if __name__ == "__main__":
     main()
